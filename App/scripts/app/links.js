d294f12fabf6e5763b6d01b6586103e3ffe38346
Brad Ash <bash@capinfogroup.com>
2014-03-09 11:24:31 -0500

    Fixed data binding bugs with link view and styles

--- App/scripts/app/links.js
+++ App/scripts/app/links.js
@@ -54,41 +54,43 @@ app.Links = (function () {
                   var links = [];
                   for (var i = 0; i < response.data.links.length; i++) {
                       var userId = response.data.links[i].user_id;
-                            //console.debug(app.Users.users());
-                         var user = $.grep(app.Users.users(), function (e) {
-                       		return e.user_id === userId;
-                    	})[0];
+                      var user = $.grep(app.Users.users(), function (e) {
+                          return e.user_id === userId;
+                      })[0];
                       
-                      	var username = user ? user.username : 'Anonymous';
-                        var avatar = app.helper.resolveProfilePictureUrl(user);
-                      console.log(username + ' : ' + avatar);
+                      var username = user ? user.username : 'Anonymous';
+                      var avatar = app.helper.resolveProfilePictureUrl(user);
                       
-                    var link = {
-                      	'id': response.data.links[i].srl_id,
-                        'link_id': response.data.links[i].link_id,
-                        'link': response.data.links[i].link,
-                    	'title': response.data.links[i].title,
-                        //created: app.helper.formatDate(response.data.links[i].created)
-                        'created': response.data.links[i].relativetime,
-                        'views': response.data.links[i].views ? response.data.links[i].views : 0,
-                        'votes': response.data.links[i].numvotes  ? response.data.links[i].numvotes  : 0,
-                        'voteStatus': response.data.links[i].user_voted == 1 ? 'on' : '',
-                        'user_id': userId,
-                        'username': username,
-                        'avatar': avatar
-                    };
-                    links.push(link);
+                      var link = {
+                          'id': response.data.links[i].srl_id,
+                          'link_id': response.data.links[i].link_id,
+                          'link': response.data.links[i].link,
+                          'title': response.data.links[i].title,
+                          //created: app.helper.formatDate(response.data.links[i].created)
+                          'created': response.data.links[i].relativetime,
+                          'views': response.data.links[i].views ? response.data.links[i].views : 0,
+                          'votes': response.data.links[i].numvotes  ? response.data.links[i].numvotes  : 0,
+                          'voteStatus': response.data.links[i].user_voted == 1 ? 'on' : '',
+                          'user_id': userId,
+                          'username': username,
+                          'avatar': avatar
+                      };
+                      links.push(link);
                   }
                   return links;
                 }
             },
-            change: function (e) {
-                
+            change: function (e) { 
                 if (e.items && e.items.length > 0) {
                     $('#no-links-span').hide();
                 } else {
                     $('#no-links-span').show();
                 }
+                
+                // create a template instance
+                var template = kendo.template($("#linkTemplate").html());
+                // render a view by passing the data to a template
+                kendo.render(template, this);
             }
         });
        
@@ -103,17 +105,20 @@ app.Links = (function () {
         
          var linkVote = function (e) {
              var data = e.data;
+             var target = e.touch.target;
              var voteup = e.data.voteStatus == '' ? true : false;
-              
+             var votes = parseInt(data.votes);
+             
          	$.getJSON(appSettings.api.url + '/link/vote?callback=?', { 
                 guid: window.localStorage.getItem("guid"),
              	srlkey: appSettings.api.srlKey,
                 srlid: e.data.id
             }).done(function(response) {
                 if (response.data) {
-                  if (voteup) data.votes++;  //New vote so incremement vote count
-                  else if (parseInt(data.votes) > 0) data.votes--; //Vote down so remove vote if counter is > 0
+                  if (voteup) data.set('votes', votes + 1);  //New vote so incremement vote count
+                  else if (parseInt(data.votes) > 0) data.set('votes', votes - 1); //Vote down so remove vote if counter is > 0
                  
+                  target.find('a').toggleClass('on');
                   data.set('voteStatus', voteup ? 'on' : '');
                 }
             }).fail(function( jqxhr, textStatus, error ) {
@@ -123,7 +128,9 @@ app.Links = (function () {
         
         // Navigate to linkView When some link is selected
         var linkSelected = function (e) {
-            var linkUrl = e.data.link;
+            var data = e.data;
+            var linkUrl = data.link;
+            var views = parseInt(data.views);
             
             $.getJSON(appSettings.api.url + '/link/increment-view?callback=?', { 
                 guid: window.localStorage.getItem("guid"),
@@ -133,6 +140,7 @@ app.Links = (function () {
                 
             }).always(function() { //Redirect even if the increment fails
                var ref = window.open(linkUrl, "_blank", 'location=yes'); 
+                data.set('views', views+1); //Increment view counter
                 /*ref.addEventListener('loadstart', function() {
                 ref.insertCSS({
                         code: "body { background: #fff; }" 
